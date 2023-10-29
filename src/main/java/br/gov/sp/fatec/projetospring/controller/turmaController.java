80b4dc0bc775f362f668a6f182fb1a5e352c85f0
Ana Silva <Anachr870@gmail.com>
2023-10-29 19:27:08 +0000

    Comitando funções e rotas funcionando

--- src/main/java/br/gov/sp/fatec/projetospring/controller/turmaController.java
+++ src/main/java/br/gov/sp/fatec/projetospring/controller/turmaController.java
@@ -1,5 +1,4 @@
 package br.gov.sp.fatec.projetospring.controller;
-
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,10 +6,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.gov.sp.fatec.projetospring.entity.Turma;
+import br.gov.sp.fatec.projetospring.service.turmaService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 
 
@@ -39,4 +43,3 @@ public class turmaController {
         return new ResponseEntity<>(novaTurma, HttpStatus.CREATED);
     }
 }
-
--- src/main/java/br/gov/sp/fatec/projetospring/service/turmaService.java
+++ src/main/java/br/gov/sp/fatec/projetospring/service/turmaService.java
@@ -2,23 +2,49 @@ package br.gov.sp.fatec.projetospring.service;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import br.gov.sp.fatec.projetospring.entity.Turma;
-///
+import br.gov.sp.fatec.projetospring.repository.turmaRepository;
 
-
-public interface turmaRepository extends JpaRepository<Turma, Long> {
-
-    @Query("select t from Turma t where t.ano = 2023")
-    public Optional<Turma> buscarPorano(Integer ano);
+//questao 3 - 3 metodos um para cadastro, um para buscar todos registros e um para buscar turma
+@Service
+public class turmaService {
     
-    
-//   /turma/buscarPorAnoETamanho?ano=2023&tamanho=40
-    @Query("select t from Turma t where t.ano = :anoParam and t.tamanho < :tamanhoParam")
-    List<Turma> findTurmasByAnoAndTamanho(@Param("anoParam") Integer ano, @Param("tamanhoParam") Integer tamanho);
+    @Autowired
+    private turmaRepository turmaRepo;
+
+    //listando todos os registros;
+    public List<Turma> todasAsTurmas() {    
+        return turmaRepo.findAll();
+    }
+
+    //buscando por ano, consulta anterior. Rota de paramentros esta no repository 
+    public List<Turma> buscarTurmasPorAnoETamanho(Integer ano, Integer tamanho) {
+        return turmaRepo.findTurmasByAnoAndTamanho(ano, tamanho);
+    }
+
+    //Cadastro de turmas :D
+    public Turma cadastrarTurma(Turma turma) {
+        return turmaRepo.save(turma);
+    }
+
+    /*para cadastrar, 
+    1 - acessa o link https://www.webtools.services/online-rest-api-client, 
+    2 - adicionar tipo de metodo, no caso do cadastro é metodo POST
+    3 - cole a rota da aplicação do metodo de cadastrarTurma()
+    4 - Em request settings, ir em BODY selecionar 'raw', selecionar json aplication. Logo abaixo colocar seguinte
+    json ou algo parecido:
+     
+        {
+            "ano": 2023,
+            "semestre": 1,
+            "tamanho": 30,
+            "data_hora_cadastro": "2023-10-29 14:00" 
+        }
+
+    5 - clicar em SEND, e ta pronto o sorvetinho!😄🍦
+
+     */
 }
